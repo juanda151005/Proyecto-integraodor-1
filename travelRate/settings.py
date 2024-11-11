672b61fc939704bee40909cc87e2ea8911070789
Bayron Mena <bayronmenar@gmail.com>
2024-11-11 13:10:12 -0500

    Deployment

--- travelRate/settings.py
+++ travelRate/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-1^dt-2y7liog*^vjvx+$#tc&9*50-tjcl8-1n566k3vp9ehpt1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.57.168.21']
 
 
 # Application definition
